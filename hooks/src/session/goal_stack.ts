18b6d779ac555a045dc75b0d6735832b87f97831
Cody Arnold <codya@atgfw.com>
2026-02-05 14:13:50 -0800

    chore(sync): session state sync

--- hooks/src/session/goal_stack.ts
+++ hooks/src/session/goal_stack.ts
@@ -10,7 +10,7 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 import * as crypto from 'node:crypto';
-import { getClaudeDir } from '../utils.js';
+import { getClaudeDir, isPathMatch } from '../utils.js';
 import type { HookInput } from '../types.js';
 
 // ============================================================================
